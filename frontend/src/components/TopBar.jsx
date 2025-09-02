ef7a63c62a445dbe75a8ed21bbb25adb3d8f7ce7
Thibault Pouch <thibault.pouch@epitech.eu>
2025-09-02 19:51:38 +0200

    [feat] add home icon and update TopBar component with new branding

--- frontend/src/components/TopBar.jsx
+++ frontend/src/components/TopBar.jsx
@@ -1,8 +1,14 @@
+import homeIcon from '../assets/home.svg'
+import './TopBar.css'
+
 export default function TopBar() {
   return (
     <header className="topbar">
       <div className="topbar-left">
-        <h1 className="brand">NextRise</h1>
+        <h1 className="brand">JEB Incubator</h1>
+        <a href="/" className="home-link" aria-label="Aller à l'accueil">
+          <img src={homeIcon} alt="Accueil" className="home-icon" />
+        </a>
       </div>
       <div className="topbar-right">
         <input className="search" placeholder="Search..." />

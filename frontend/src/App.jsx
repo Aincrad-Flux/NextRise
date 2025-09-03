4844d6ddc4a5fd393e681ab7f984b4899a95c586
Antoine Papillon <papillon.antoine59@gmail.com>
2025-09-03 10:41:44 +0200

    [feat] add new routes for Projects, News, Events, Search, and About pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,11 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import StartupHome from './pages/StartupHome.jsx'
+import Projects from './pages/Projects.jsx'
+import News from './pages/News.jsx'
+import Events from './pages/Events.jsx'
+import Search from './pages/Search.jsx'
+import About from './pages/About.jsx'
 import NotFound from './pages/NotFound.jsx'
 
 function App() {
@@ -10,6 +15,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/startup" element={<StartupHome />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/advanced-search" element={<Search />} />
+        <Route path="/about" element={<About />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>

a06be0e28d842fc69721001eb7d4bba438699b63
clementaugustinowick <clement.augustinowick@epitech.eu>
2025-09-03 10:14:20 +0200

    [feat] add media query in navbar css to adjust navbar style to each screen size

--- frontend/src/navBar/navBar.jsx
+++ frontend/src/navBar/navBar.jsx
@@ -3,36 +3,43 @@ import './navBar.css'
 
 function NavBar(){
 
-  const [menuBar, setMenu] = useState(true)
   const isStartup = false;
   const isAdmin = false;
   const isLoggedIn = false
 
   return (
-    <nav className='menu'>
-      <ul className={`${menuBar ? 'flex' : 'hidden'} 'w-full flex-col items-center justify-center bg-slate-900 absolute top-full pb-5 sm:flex sm:relative sm:flex-row sm:pb-0'`}>
-        <li>
-          <a className='menuLink' href="#">Home</a>
-        </li>
-        <li>
-          <a className='menuLink' href="#">Project catalog</a>
-        </li>
-        <li>
-          <a className='menuLink' href="#">News</a>
-        </li>
-        <li>
-          <a className='menuLink' href="#">Events</a>
-        </li>
-        {isStartup && <li><a className='menuLink' href="#">Startup</a></li>}
-        {isAdmin && <li><a className='menuLink' href="#">Admin</a></li>}
-        {
-          isLoggedIn ?
-          (<li><a className='menuLink' href="#">Profil</a></li>)
-          :
-          (<li><a className='menuLink' href="#">Log in</a></li>)
-        }
-      </ul>
-    </nav>
+    <div>
+      <button className='list-icon'>
+        <img src="../assets/react.svg" alt=''/>
+      </button>
+      <button className='arrow-left-icon'>
+        <img src="../assets/arrow-left.svg" alt=''/>
+      </button>
+      <nav className='menu'>
+        <ul>
+          <li>
+            <a className='menuLink' href="#">Home</a>
+          </li>
+          <li>
+            <a className='menuLink' href="#">Project catalog</a>
+          </li>
+          <li>
+            <a className='menuLink' href="#">News</a>
+          </li>
+          <li>
+            <a className='menuLink' href="#">Events</a>
+          </li>
+          {isStartup && <li><a className='menuLink' href="#">Startup</a></li>}
+          {isAdmin && <li><a className='menuLink' href="#">Admin</a></li>}
+          {
+            isLoggedIn ?
+            (<li><a className='menuLink' href="#">Profil</a></li>)
+            :
+            (<li><a className='menuLink' href="#">Log in</a></li>)
+          }
+        </ul>
+      </nav>
+    </div>
   )
 }
 
